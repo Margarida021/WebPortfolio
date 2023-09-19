9fca116ae942099c3a059b9d86b7a4d9c16d2f09
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-09-19 17:55:40 +0100

    Added nav bar, sections and some general content

---
+++ index.js
@@ -0,0 +1,15 @@
+// navbar active on scroll
+window.addEventListener('scroll', () => {
+  navbar = document.querySelector('.navbar');
+  navbar.classList.add('active');
+});
+
+// navbar item active on section
+
+// nagivation dots active on section
+const sections = document.querySelectorAll('section');
+const bullets = document.querySelector('.navigation');
+
+// job-title changing on load
+
+// items appear on scroll section
