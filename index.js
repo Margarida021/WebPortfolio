ee8d510350c2d85e46873a61ad20673b559c0071
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-09-22 21:28:45 +0100

    navbar javascript

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // navbar active on scroll
 window.addEventListener('scroll', () => {
   navbar = document.querySelector('.navbar');
-  navbar.classList.add('active');
+  if (window.scrollY > 50) {
+    navbar.classList.add('navbar-scrolled');
+  } else if (window.scrollY <= 50) {
+    navbar.classList.remove('navbar-scrolled');
+  }
 });
 
 // navbar item active on section
@@ -9,15 +13,3 @@ window.addEventListener('scroll', () => {
 // nagivation dots active on section
 const sections = document.querySelectorAll('section');
 const bullets = document.querySelector('.navigation');
-
-// job-title changing on load
-
-// items appear on scroll section
-
-
-<script>
-
-ScrollReveal().reveal('.about-me', 'p-aboutme' { delay: 500 });
-ScrollReveal().reveal('.tagline', { delay: 500 });
-ScrollReveal().reveal('.tagline', { delay: 500 });
-</script>
---
+++ scrollReveal.js
@@ -0,0 +1,21 @@
+
+ScrollReveal({ reset: true });
+
+// section 1
+ScrollReveal().reveal('.section-one', { delay: 90 });
+
+// section 2
+ScrollReveal().reveal('.about-me', { delay: 150 });
+ScrollReveal().reveal('.p-aboutme', { delay: 150 });
+ScrollReveal().reveal('.education-section', { delay: 350 });
+ScrollReveal().reveal('.skills-section', { delay: 150 });
+
+// section 3
+ScrollReveal().reveal('.projects', { delay: 150 });
+ScrollReveal().reveal('.p-projects', { delay: 150 });
+ScrollReveal().reveal('.projects-container', { delay: 350 });
+
+// section 4
+ScrollReveal().reveal('.contacts', { delay: 150 });
+ScrollReveal().reveal('.p-contacts', { delay: 150 });
+ScrollReveal().reveal('.contact-list', { delay: 350 });
