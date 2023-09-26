295fae551fb06f1fe39e33a1c9926f2179d9a8fe
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-09-26 12:45:22 +0100

    navdots on the side active on each section with click and scroll

--- Script/index.js
+++ Script/index.js
@@ -8,76 +8,26 @@ window.addEventListener('scroll', () => {
   }
 });
 
-// navbar item active on section
+// navbar item active on section // navdots active on section
 let sections = document.querySelectorAll('section');
 let navItems = document.querySelectorAll('.navbar-list a');
+let navDots = document.querySelectorAll('.navigation a');
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach(section => {
     let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute('id');
     if(top >= offset && top < offset + height) {
       navItems.forEach(items => {
         items.classList.remove('active');
-        document.querySelector('.navbar-list a[href*='+ id + ']').classList.add('active');
+        document.querySelector('.navbar-list a[href*='+ id +']').classList.add('active');
+      });
+      navDots.forEach(link => {
+        link.classList.remove('selected');
+        document.querySelector('.navigation a[href*=' + id + ']').classList.add('selected');
       });
     };
   });
 };
-
-// nagivation dots active on section
-// const sections = document.querySelectorAll('section');
-// const bullets = document.querySelector('.navigation');
-
-
-
-//navbar - hover e "page active" (scroll)
-// const navLinkEls = document.querySelectorAll('.nav_link');
-// const sectionEls = document.querySelectorAll('section');
-
-// let currentSection = 'home';
-// window.addEventListener('scroll', () => {
-//      sectionEls.forEach(sectionEl => {
-//         if (window.scrollY >= (sectionEl.offsetTop - 170)) {
-//             currentSection = sectionEl.id;
-//         }
-// });
-//     navLinkEls.forEach(navLinkEl => {
-//         if (navLinkEl.href.includes(currentSection)) {
-//             document.querySelector('.active').classList.remove('active');
-//             navLinkEl.classList.add('active');
-//         };
-//     });
-// });
-
-//dots laterais de navegação - hover e "page active" (scroll)
-// const sections = document.querySelectorAll('section');
-// const windowHeight = window.innerHeight;
-// const navigation = document.querySelector('.navigation');
-
-// function reset() {
-//     for (let i = 0; i < navigation.children.length; i++) {
-//         navigation.children[i].classList.remove('selected');
-//     }
-// }
-// window.addEventListener('scroll', function() {
-//     const scrollTop = window.scrollY;
-//     sections.forEach(function(section, i) {
-//         if (section.offsetTop < scrollTop + windowHeight/2 && scrollTop < section.offsetTop + windowHeight/2) {
-//             reset();
-//             navigation.children[i].classList.add('selected');
-//         }
-//     })
-// });
-
-//dots laterais (click)
-// document.querySelectorAll('.navigation li').forEach(function(item, i) {
-//     item.addEventListener('click', function() {
-//         window.scrollTo({
-//             top: i * windowHeight,
-//             behavior: 'smooth',
-//         })
-//     })
-// });
